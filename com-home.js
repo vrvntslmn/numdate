75b7bad36b55a55cec36b9dbc804e01ddb55a263
anudari <aaannnuuuxxx@gmail.com>
2025-12-04 11:52:56 +0800

    Сонирхол
    anu

--- com-home.js
+++ com-home.js
@@ -841,7 +841,7 @@ class Home extends HTMLElement {
                     </div>
                     <div class="dropdown">
                         <button class="dropbtn">
-                            <h1>Хобби</h1>
+                            <h1>Сонирхол</h1>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="M7 10l5 5 5-5H7z" />
                             </svg>
