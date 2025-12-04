3318c1d8a2944aff4360a6fbe0bead966e69a8e0
D.Temuujin <23b1num0898@stud.num.edu.mn>
2025-12-04 10:18:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -158,6 +158,7 @@ class App extends HTMLElement {
                 <li><a href="#/">Home</a></li>
                 <li><a href="#dateidea">Date idea</a></li>
                 <li><a href="#messenger">Messages</a></li>
+                <li><a href="#profile">Profile</a></li>
                 <li>
                     <a href="#profile">
                         <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
