fbd9d5f6d88daba2f07b7bf2729107d1eee3f199
vrvntslmn <nuureeuuree@gmail.com>
2025-12-18 09:45:05 +0800

    Update com-home.js

--- com-home.js
+++ com-home.js
@@ -878,6 +878,7 @@ class Home extends HTMLElement {
             console.error('Failed to load profiles:', err);
         });
 
+        
         class ProfileSwipe {
             constructor() {
                 this.currentProfileIndex = 0;
